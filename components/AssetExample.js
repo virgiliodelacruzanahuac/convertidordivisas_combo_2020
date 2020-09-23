dd0e7234935b61647b3ab0cca9b50592ce6a1bcd
virgiliodelacruzanahuac <70555842+virgiliodelacruzanahuac@users.noreply.github.com>
2020-09-22 19:14:14 -0500

    Add files via upload

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
-export default class AssetExample extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Local files and assets can be imported by dragging and dropping them into the editor
-        </Text>
-        <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
-      </View>
-    );
-  }
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Local files and assets can be imported by dragging and dropping them into the editor
+      </Text>
+      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
